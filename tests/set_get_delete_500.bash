d6855690f0ed2aa351258e263dfda4679ed04593
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2015-09-26 15:07:28 -0400

    Namespace the JSONLITE_PATH so that we do not clobber an existing database
    Also cleanup after ourselves by calling drop at the end

--- tests/set_get_delete_500.bash
+++ tests/set_get_delete_500.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail; [[ $TRACE ]] && set -x
 
+export JSONLITE_PATH="$(dirname "$0")/jsonlite.data"
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../jsonlite.bash"
 ITERATIONS=500
 
@@ -11,3 +12,5 @@ for i in $(seq 1 $ITERATIONS); do
   jsonlite_get "$ID"
   jsonlite_delete "$ID"
 done
+
+jsonlite_drop --force
