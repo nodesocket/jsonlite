9f16609730b012d88c8718bd16f64eb9e26611fc
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2015-09-26 14:53:04 -0400

    Rename test shell script to bash script to clarify it is written in bash
    Also use library version of jsonlite when running the test

---
+++ tests/set_get_delete_500.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eo pipefail; [[ $TRACE ]] && set -x
+
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../jsonlite.bash"
+ITERATIONS=500
+
+jsonlite_drop --force
+
+for i in $(seq 1 $ITERATIONS); do
+    ID=$(jsonlite_set '{"name":"John Doe","active":true,"permissions":{"read":true,"write":false}}')
+    jsonlite_get "$ID"
+    jsonlite_delete "$ID"
+done
