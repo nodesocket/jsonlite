3ac8acc6225dd62db483e7cfc378014443d36cb0
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2015-09-26 15:06:55 -0400

    Use two spaces instead of four

--- tests/set_get_delete_500.bash
+++ tests/set_get_delete_500.bash
@@ -7,7 +7,7 @@ ITERATIONS=500
 jsonlite_drop --force
 
 for i in $(seq 1 $ITERATIONS); do
-    ID=$(jsonlite_set '{"name":"John Doe","active":true,"permissions":{"read":true,"write":false}}')
-    jsonlite_get "$ID"
-    jsonlite_delete "$ID"
+  ID=$(jsonlite_set '{"name":"John Doe","active":true,"permissions":{"read":true,"write":false}}')
+  jsonlite_get "$ID"
+  jsonlite_delete "$ID"
 done
