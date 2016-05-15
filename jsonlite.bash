e08ec6b98f2e99ed313613c95159bd89271fd1f8
Justin <justin@commando.io>
2016-05-14 22:57:20 -0700

    added Apache license to top of jsonlite.bash

--- jsonlite.bash
+++ jsonlite.bash
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+
+################################################################################
+# Copyright 2016 Justin Keller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
 set -eo pipefail; [[ $TRACE ]] && set -x
 
 readonly VERSION="0.7.0"
@@ -92,7 +109,7 @@ jsonlite_count() {
   local count
   count=$(find "$JSONLITE_PATH" -type f | wc -l)
 
-  # piping to xargs is a trick to trim (remove leading and trailing whitespace)
+  # piping to xargs is a trick to trim (remove leading & trailing whitespace)
   echo "$count" | xargs
 }
 
